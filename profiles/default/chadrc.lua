838b753f4e0b19c5245d67fd8b3122d09d358234
devclyde <israel.devclyde@protonmail.com>
2022-05-09 21:53:43 -0300

    feat: setup tags

--- profiles/default/chadrc.lua
+++ profiles/default/chadrc.lua
@@ -2,7 +2,9 @@ local M = {}
 
 M.signals = {
   require('profiles.default.signals.naughty.display-error'),
-  require('profiles.default.signals.naughty.display')
+  require('profiles.default.signals.naughty.display'),
+
+  require('profiles.default.tag.default-layout')
 }
 
 return M
