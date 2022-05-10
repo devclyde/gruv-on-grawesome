0af84603316888c96076731768d9f51cdcaa291c
devclyde <israel.devclyde@protonmail.com>
2022-05-09 21:56:38 -0300

    fix: fix tag `request::desktop_layouts`

--- profiles/default/chadrc.lua
+++ profiles/default/chadrc.lua
@@ -4,7 +4,7 @@ M.signals = {
   require('profiles.default.signals.naughty.display-error'),
   require('profiles.default.signals.naughty.display'),
 
-  require('profiles.default.tag.default-layout')
+  require('profiles.default.signals.tag.default-layout')
 }
 
 return M
